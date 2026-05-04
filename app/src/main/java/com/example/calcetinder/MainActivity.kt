b922dbcf6218a2b684037e3eed6609850d54514d
davidmerchanaltsasu <david.merchan.50786@ikasle.egibide.org>
2026-05-04 16:32:08 +0200

    mainactivity con navegacion

---
+++ app/src/main/java/com/example/calcetinder/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.calcetinder
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.calcetinder.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+        setupActionBarWithNavController(navController, AppBarConfiguration(navController.graph))
+    }
+    override fun onSupportNavigateUp() = navController.navigateUp() || super.onSupportNavigateUp()
+}
